fcefe73b926fd79ca8434554649965470f76778e
Boooy <111326794+Naminsu060531@users.noreply.github.com>
2023-08-16 20:40:32 +0900

    yes

---
+++ 08_16/Assets/Script/Unit/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Unit
+{
+    private void Update()
+    {
+        Move();
+    }
+
+    public void Move()
+    {
+        float hAxis = Input.GetAxisRaw("Horizontal");
+        float vAxis = Input.GetAxisRaw("Vertical");
+
+        transform.position += new Vector3(hAxis, 0, vAxis) * speed * Time.deltaTime;
+    }
+}
---
+++ 08_16/Assets/Script/Unit/Unit.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public enum Type { player, enemy, bullet}
+    public Type type;
+
+    public enum Target { player, enemy}
+    public Target target;
+
+    public int hp, dropGold, getScore;
+    public float speed;
+
+    public Rigidbody rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    public IEnumerator OnDamage()
+    {
+        yield return null;
+    }
+
+}
